6cae64796ba6b4253350e7e40ac7f2fb56b62c41
Jesse Hidalgo <jesse_hidalgo8@yahoo.com>
2022-11-04 16:23:07 -0400

    deleted string interpolation

--- spec/features/user_is_able_to_edit_information_spec.rb
+++ spec/features/user_is_able_to_edit_information_spec.rb
@@ -7,40 +7,40 @@ feature "user visits sign in page" do
   scenario "new user can add information" do
     visit new_user_registration_path
 
-    fill_in 'Email', with: "#{user_1.email}"
-    fill_in 'First name', with: "#{user_1.first_name}" 
-    fill_in 'Last name', with: "#{user_1.last_name}" 
-    fill_in 'Username', with: "#{user_1.username}" 
-    fill_in 'Password', with: "#{user_1.encrypted_password}" 
-    fill_in 'Password confirmation', with: "#{user_1.encrypted_password}" 
+    fill_in 'Email', with: user_1.email
+    fill_in 'First name', with: user_1.first_name 
+    fill_in 'Last name', with: user_1.last_name 
+    fill_in 'Username', with: user_1.username 
+    fill_in 'Password', with: user_1.encrypted_password 
+    fill_in 'Password confirmation', with: user_1.encrypted_password 
 
     click_button 'Sign up'
     click_on 'Profile'
 
-    click_on "Edit Information"
+    click_on "Edit Profile Information"
     
-    expect(page).to have_field('First name', with: "#{user_1.first_name}")
-    expect(page).to have_field('Last name', with: "#{user_1.last_name}")
-    expect(page).to have_field('Email', with: "#{user_1.email}")
+    expect(page).to have_field('First name', with: user_1.first_name)
+    expect(page).to have_field('Last name', with: user_1.last_name)
+    expect(page).to have_field('Email', with: user_1.email)
   end
 
   scenario "user edits and updates information" do
 
     visit new_user_registration_path
 
-      fill_in 'Email', with: "#{user_1.email}"
-      fill_in 'First name', with: "#{user_1.first_name}" 
-      fill_in 'Last name', with: "#{user_1.last_name}" 
-      fill_in 'Username', with: "#{user_1.username}" 
-      fill_in 'Password', with: "#{user_1.encrypted_password}" 
-      fill_in 'Password confirmation', with: "#{user_1.encrypted_password}" 
+      fill_in 'Email', with: user_1.email
+      fill_in 'First name', with: user_1.first_name 
+      fill_in 'Last name', with: user_1.last_name 
+      fill_in 'Username', with: user_1.username 
+      fill_in 'Password', with: user_1.encrypted_password 
+      fill_in 'Password confirmation', with: user_1.encrypted_password 
 
       click_button 'Sign up'
       click_on 'Profile'
-      click_on "Edit Information"
+      click_on "Edit Profile Information"
 
       fill_in 'First name', with: "Jeff"
-      fill_in 'Current password', with: "#{user_1.encrypted_password}"
+      fill_in 'Current password', with: user_1.encrypted_password
  
       click_on 'Update'
 
