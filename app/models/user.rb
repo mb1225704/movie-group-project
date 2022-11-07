dd967b318c319ec6b2e486ea873c38317e4fd7ff
Marcelino Barbosa <marcelino.barbosa096@gmail.com>
2022-11-07 15:05:31 -0500

    Added the ability to upload/edit image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  mount_uploader :profile_photo, ProfilePhotoUploader
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
 end
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,16 @@
+CarrierWave.configure do |config|
+  config.storage    = :aws
+  config.aws_bucket = ENV['S3_BUCKET']
+  config.aws_acl    = 'public-read'
+  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+  config.aws_attributes = {
+    expires: 1.week.from_now.httpdate,
+    cache_control: 'max-age=604800'
+  }
+
+  config.aws_credentials = {
+    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region:            ENV['AWS_REGION']
+  }
+end
