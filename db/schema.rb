4067d5ae0f8ad6f75430400865725168de150e16
Jon Pekkarinen <77648523+jonpekk@users.noreply.github.com>
2022-11-08 14:31:48 -0500

    formatting fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_01_140740) do
+ActiveRecord::Schema.define(version: 2022_11_04_190302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
