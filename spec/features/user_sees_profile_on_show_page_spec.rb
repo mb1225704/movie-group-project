8fe19b80ad1d913b71bbe9f0065858a0adc5e788
Jeffrey Giering <gieringj@gmail.com>
2022-11-08 14:08:37 -0500

    uploading profile picture

--- spec/features/user_sees_profile_on_show_page_spec.rb
+++ spec/features/user_sees_profile_on_show_page_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 feature "User sees profile on show page" do
 
-    let!(:user_1) { User.create(email: "patrickstar24@aol.com", first_name: "Patrick", last_name: "Star", profile_image_url: "https://cdn.shopify.com/s/files/1/0054/4371/5170/products/PatrickStar_467pin.png?v=1627414164", password: "password", password_confirmation: "password") }
+    let!(:user_1) { User.create(email: "patrickstar24@aol.com", first_name: "Patrick", last_name: "Star", profile_photo: "https://cdn.shopify.com/s/files/1/0054/4371/5170/products/PatrickStar_467pin.png?v=1627414164", password: "password", password_confirmation: "password") }
 
     scenario 'visit and view user_1 profile show page' do
         visit user_path(user_1)
--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -6,12 +6,6 @@ feature 'user registers', %Q{
   So that I can create an account
 } do
 
-  # Acceptance Criteria:
-  # * I must specify a valid email address,
-  #   password, and password confirmation
-  # * If I don't specify the required information, I am presented with
-  #   an error message
-
   scenario 'provide valid registration information' do
     visit new_user_registration_path
 
@@ -21,6 +15,7 @@ feature 'user registers', %Q{
     fill_in 'Username', with: 'jexample'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
+    attach_file 'user[profile_photo]', "#{Rails.root}/spec/support/images/David-Delahunty-Graphic-Design-Make-something-cool-everyday-11-594x821.jpeg"
 
     click_button 'Sign up'
 
