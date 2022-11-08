08c318aa9c9ee784028b978502bdeacd16ef0bf8
Jon Pekkarinen <77648523+jonpekk@users.noreply.github.com>
2022-11-08 16:39:34 -0500

    final validation and testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :reviews
+  validates :email, :username, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   mount_uploader :profile_photo, ProfilePhotoUploader
--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -6,6 +6,9 @@ feature 'user registers', %Q{
   So that I can create an account
 } do
 
+  let!(:user_1) { User.create(first_name: "test", last_name: "user", username: "tester", email: "test@test.com", password: "testtest")
+  }
+  
   scenario 'provide valid registration information' do
     visit new_user_registration_path
 
@@ -25,9 +28,24 @@ feature 'user registers', %Q{
 
   scenario 'provide invalid registration information' do
     visit new_user_registration_path
-
+  
     click_button 'Sign up'
     expect(page).to have_content("can't be blank")
+    expect(page).to_not have_content('Sign Out')
+  end
+
+  scenario 'provides duplicated values where they cannot exist' do
+    visit new_user_registration_path
+    
+    fill_in 'Email', with: 'test@test.com'
+    fill_in 'First name', with: 'John' 
+    fill_in 'Last name', with: 'Example'
+    fill_in 'Username', with: 'jexample'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+
+    click_button 'Sign up'
+    expect(page).to have_content("Email has already been taken")
     expect(page).to_not have_content('Sign Out')
   end
 end
